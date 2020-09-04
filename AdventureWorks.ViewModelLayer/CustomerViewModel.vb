060a00e035e5c6d6fd2d63df905adf05241ee3c4
Samuel Ketels <ketels.samuel@gmail.com>
2020-09-04 17:29:06 +0200

    Add project files.

---
+++ AdventureWorks.ViewModelLayer/CustomerViewModel.vb
@@ -0,0 +1,17 @@
+﻿Imports Common.Library
+Imports AdventureWorks.EntityLayer
+
+Public Class CustomerViewModel
+    Inherits ViewModelBase
+
+    Sub New()
+        LoadCustomer(1)
+    End Sub
+    Public Property Entity As Customer
+
+    Function LoadCustomer(ByVal customerId As Integer) As Customer
+        Entity = New Customer() With {.CustomerId = 1, .FirstName = "Bruce", .LastName = "Wayne", .CompanyName = "Wayne Enterprises"}
+
+        Return Entity
+    End Function
+End Class
---
+++ AdventureWorks.ViewModelLayer/ProductViewModel.vb
@@ -0,0 +1,22 @@
+﻿Imports Common.Library
+Imports AdventureWorks.EntityLayer
+
+Public Class ProductViewModel
+    Inherits ViewModelBase
+
+    Sub New()
+        LoadProduct(680)
+    End Sub
+
+    Public Property Entity As Product
+
+    Function LoadProduct(ByVal productId As Integer) As Product
+        Return LoadProduct(productId, Nothing)
+    End Function
+
+    Function LoadProduct(ByVal productId As Integer, ByVal startingFilePath As String) As Product
+        'Hardcode an entity
+        Entity = New Product() With {.ProductId = 680, .Name = "HL Road Frame - Black, 58", .ProductNumber = "FR-R92B-58", .Color = "Black", .Size = "58", .Weight = 1016.04D, .StandardCost = 1059.31D, .ListPrice = 1431.5D, .SellStartDate = #6/1/1998 12:00:00 AM#, .SellEndDate = #12/31/2021 12:00:00 AM#}
+        Return Entity
+    End Function
+End Class
